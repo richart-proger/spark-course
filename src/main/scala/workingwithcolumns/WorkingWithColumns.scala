0ac8b6dff667d70b576d99747ceca69f263b3ced
Artur Galimov <satorica@mail.ru>
2022-07-16 22:03:08 +0300

    other methods when working with columns

--- src/main/scala/workingwithcolumns/WorkingWithColumns.scala
+++ src/main/scala/workingwithcolumns/WorkingWithColumns.scala
@@ -44,5 +44,39 @@ object WorkingWithColumns extends App {
 
   bikeSharingDF.select("Date", "Hour")
 
+//  Считаем общее количество записей
+  val bikes = bikeSharingDF.select("*").count()
+  println(s"number of records $bikes") // number of records 8760
+
+
+//  Считаем количество уникальных значений в колонке HOLIDAY
+  val distinctEntries = bikeSharingDF.select("HOLIDAY").distinct().count()
+  println(s"distinct values in column HOLIDAYS  $distinctEntries") // distinct values in column HOLIDAYS  2
+
+//  Переименовываем колонку TEMPERATURE в Temp
+  val bikesWithColumnRenamed = bikeSharingDF.withColumnRenamed("TEMPERATURE", "Temp")
+  bikesWithColumnRenamed.show(3)
+
+//  Удаляем колонки Date и Hour
+  bikeSharingDF.drop("Date", "Hour")
+
+
+//    Создаем колонку is_holiday, которая будет состоять из значений true (если значение в колонке HOLIDAY равно Holiday), false (если значение в колонке HOLIDAY равно No Holiday), null (если значение в колонке HOLIDAY иное чем Holiday или No Holiday)
+  bikeSharingDF
+    .withColumn(
+      "is_holiday",
+      when(col("HOLIDAY") === "No Holiday", false)
+        .when(col("HOLIDAY") === "Holiday", true)
+        .otherwise(null)
+    )
+
+//  для каждой даты Date рассчитываем сумму взятых в аренду велосипедов.  Результат записываем в колонку bikes_total. Конечный датафрейм сортируем по дате посредством orderBy:
+    bikeSharingDF
+    .groupBy("Date")
+  .agg(
+    sum("RENTED_BIKE_COUNT").as("bikes_total"))
+    .orderBy("Date")
+    .show(3)
+
   spark.stop()
 }
